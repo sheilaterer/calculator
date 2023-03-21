1d56945332453ed21b79a95e780cdf2a476914a6
sheilaterer <127751878+sheilaterer@users.noreply.github.com>
2023-03-21 16:16:22 +0300

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,15 @@
+# program for simple calculator
+firstnumber = float(input("Enter first number:"))
+secondnumber = float(input("Enter second number"))
+myoperator = str(input("Enter operator(+, -, *, /)"))
+
+if myoperator=="+":
+    print(firstnumber+secondnumber)
+elif myoperator=="-":
+    print(firstnumber-secondnumber)
+elif myoperator=="/":
+    print(firstnumber/secondnumber)
+elif myoperator=="*":
+    print(firstnumber*secondnumber)
+else:
+    print("Invalid")
